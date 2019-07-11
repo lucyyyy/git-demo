4153225319c39515ef28cf54f0a624431ee76365
Lucy <644156399@qq.com>
2019-07-11 09:10:59 +0800

    Add trial.py

---
+++ trial.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import platform 
+a = 0
+for i in range(9999):
+    a += i
+print("Finish job, result=%i" % a)
+print("This is", platform.system())
+
